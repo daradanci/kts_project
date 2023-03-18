8b4a50db2fe8d136ebf6d78f8d4fca5231991251
Danil ZelinskiyД <zelinskiy.danya@list.ru>
2023-03-18 14:55:05 +0300

    vk echo bot

---
+++ kts_backend/store/database/database.py
@@ -0,0 +1,43 @@
+from typing import Optional, TYPE_CHECKING, Callable
+
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, create_async_engine
+from sqlalchemy.orm import declarative_base, Session,sessionmaker
+from sqlalchemy import select, text,exc
+from app.quiz.models import Theme, ThemeModel
+from app.store.database import db
+if TYPE_CHECKING:
+    from app.web.app import Application
+
+
+class Database:
+    def __init__(self, app: "Application"):
+        self.app = app
+        self._engine: Optional[AsyncEngine] = None
+        self._db: Optional[declarative_base] = None
+        self.session: Optional[Callable[[], AsyncSession]] = None
+
+
+    async def connect(self, *_: list, **__: dict) -> None:
+        self._db = db
+        # echo=True,future=True
+        self._engine = create_async_engine('postgresql+asyncpg://kts_user:kts_pass@localhost/kts')
+        print('CONNECTED to database successfully')
+        self.session=sessionmaker(self._engine,expire_on_commit=False, class_=AsyncSession)
+
+
+        # with self.session as session:
+
+
+            # stmt=text("""insert into themes (title) values ('web7777')""")
+            # res=await self.session.execute(stmt)
+            # await self.session.commit()
+
+
+        # async with self._engine.connect() as conn:
+        #     await conn.execute(text("""insert into themes (title) values ('web37722')"""))
+        #     await conn.commit()
+
+    async def disconnect(self, *_: list, **__: dict) -> None:
+        # self.session.expunge_all()
+        if self._engine:
+            await self._engine.dispose()
